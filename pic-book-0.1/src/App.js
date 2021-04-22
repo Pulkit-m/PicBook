56519f777ed19ccbbc18d2af3b4c09999688f424
Saumya <saumya31626@gmail.com>
2021-04-23 00:31:15 +0530

    Initial Frontend Design

---
+++ pic-book-0.1/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
+import Navbar from './Components/Navbar';
+import Home from './Components/Pages/Home';
+import Setting from './Components/Pages/Setting';
+import User from './Components/Pages/User';
+import Upload from './Components/Pages/Upload';
+import LogOut from './Components/Pages/Log-out';
+
+
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/Setting' component={Setting} />
+          <Route path='/User' component={User} />
+          <Route path='/Upload' component={Upload} />
+          <Route path='/Log-out' component={LogOut} />
+        </Switch>
+      </Router>
+    </>  
+  );
+}
+
+export default App;
